aa59f720b60ca4bf2374b1541ba28d816783ab3d
sven12301 <sven.bulach@web.de>
2022-07-28 11:42:01 +0200

    Changed inference.py structure, added image cropping

--- darknet/call_inference_example.py
+++ darknet/call_inference_example.py
@@ -1,6 +1,16 @@
 from inference import Inference
+import cv2
+
+# This is just for test and show purposes
 
 inf = Inference()
+i=0
+while True:
+	i+=1
+	classes, cropped_imgs, x_centers = inf.inference()
+	if i%20 ==0:
+		for crop_img in cropped_imgs:
 
-for i in range(100):
-	print(inf.inference())
+			cv2.imshow("cropped img", crop_img)
+			cv2.waitKey(0)
+
--- darknet/inference.py
+++ darknet/inference.py
@@ -1,48 +1,30 @@
 from darknet import *
 import cv2
 import jetson.utils
+import random 
+import time
 
 class Inference:
 
-    def __init__(self,width = 1280, height=720):
+    def __init__(self):
         self.cfg_path = '/home/nvidia/Projects/darknet/cfg/custom-yolov4-tiny.cfg'
         self.coco_data_path = '/home/nvidia/Projects/darknet/cfg/coco.data'
         self.weights_path = '/home/nvidia/Projects/darknet/yolov4-tiny.weights'
-        self.detections = None
+        self.thresh = 0.5
         self.cap = cv2.VideoCapture(self.gstreamer_pipeline(
                             capture_width=416, capture_height=416,
                             flip_method=0),cv2.CAP_GSTREAMER)
         self.network, self.class_names, self.class_colors = load_network(self.cfg_path, self.coco_data_path, self.weights_path)
-        self.width = network_width(self.network)
-        self.height = network_height(self.network)
-
-
-
-    # darknet helper function to run detection on image
-    def darknet_helper(self, img, width, height):
-        darknet_image = make_image(width, height, 3)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_resized = cv2.resize(img_rgb, (width, height),
-                                    interpolation=cv2.INTER_LINEAR)
-
-        # get image ratios to convert bounding boxes to proper size
-        img_height, img_width, _ = img.shape
-        width_ratio = img_width/width
-        height_ratio = img_height/height
-
-        # run model on darknet style image to get detections
-        copy_image_from_bytes(darknet_image, img_resized.tobytes())
-        detections = detect_image(self.network, self.class_names, darknet_image)
-        free_image(darknet_image)
-        return detections, width_ratio, height_ratio
+        self.darknet_width =network_width(self.network)
+        self.darknet_height =network_height(self.network)
 
 
     def gstreamer_pipeline(self,
         sensor_id=0,
-        capture_width=1920,
-        capture_height=1080,
-        display_width=960,
-        display_height=540,
+        capture_width=416,
+        capture_height=416,
+        display_width=416,
+        display_height=416,
         framerate=30,
         flip_method=0,
     ):
@@ -65,27 +47,167 @@ class Inference:
         )
 
 
+    def crop(self,img,detections):
+        for i in range(len(detections)):
 
+            x_min = int(detections[i][2][2])
+            x_max = int(detections[i][2][3])
+            y_min = int(detections[i][2][0])
+            y_max = int(detections[i][2][1])
+            print(x_min,x_max,y_min,y_max)
+            crop_img = img[x_min:x_max,y_min:y_max]
+            cv2.imshow("cropped_img", crop_img)
+            cv2.waitKey(0)
+
+
+    def conv_crop1(self,img, detections):
+        img_height, img_width, _ = img.shape
+        for i in range(len(detections)):
+            x_min = int(detections[i][2][0])
+            x_max = int(x_min + detections[i][2][2])
+            y_max = int(detections[i][2][1])
+            y_min = int(y_max -detections[i][2][3])
+            crop_img = img[x_min:x_max,y_min:y_max]
+            cv2.imshow("cropped img", crop_img)
+            cv2.waitKey(0)
+
+
+    def conv_crop(self, img, bbox):
+        """Crops a image depending on bounding box 
+
+        Parameter
+        ---------
+        img: array
+            The image that needs to be cropped
+        bbox: list
+            The Bounding Box that should be cropped out of image
+            with format: [x_center, y_center, bbox_widht, bbox_heigth]
+
+        Returns
+        -------
+        image:array
+            The cropped image
+            
+        """
+
+        x, y, w, h = bbox
+        
+        image_h, image_w, __ = img.shape
+
+        left = int(x - w / 2.)
+        right = int(x + w / 2.)
+        top = int(y + h / 2.)
+        bottom = int(y - h / 2.)
+
+        if (left < 0):
+            left = 0
+        if (right > image_w - 1):
+            right = image_w - 1
+        if (top > image_h-1):
+            top = image_h-1
+        if (bottom < 0):
+            bottom = 0
+
+        crop_img = img[bottom:top,left:right]
+        
+        return crop_img
+    
+
+     
+    def convert2relative(self, bbox):
+        """
+        YOLO format use relative coordinates for annotation
+        """
+        x, y, w, h = bbox
+        _height = self.darknet_height
+        _width = self.darknet_width
+        return x/_width, y/_height, w/_width, h/_height
+
+
+    def convert2original(self, image, bbox):
+        x, y, w, h = self.convert2relative(bbox)
+
+        image_h, image_w, __ = image.shape
+
+        orig_x = int(x * image_w)
+        orig_y = int(y * image_h)
+        orig_width = int(w * image_w)
+        orig_height = int(h * image_h)
+
+        bbox_converted = (orig_x, orig_y, orig_width, orig_height)
+
+        return bbox_converted
+
+
+    def convert4cropping(self, image, bbox):
+        x, y, w, h = self.convert2relative(bbox)
+
+        image_h, image_w, __ = image.shape
+
+        orig_left = int((x - w / 2.) * image_w)
+        orig_right = int((x + w / 2.) * image_w)
+        orig_top = int((y - h / 2.) * image_h)
+        orig_bottom = int((y + h / 2.) * image_h)
+
+        if (orig_left < 0):
+            orig_left = 0
+        if (orig_right > image_w - 1):
+            orig_right = image_w - 1
+        if (orig_top < 0):
+            orig_top = 0
+        if (orig_bottom > image_h - 1):
+            orig_bottom = image_h - 1
+
+        bbox_cropping = (orig_left, orig_top, orig_right, orig_bottom)
+        print(bbox_cropping)
+        return bbox_cropping
+    
+
+    def video_capture(self, frame):
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (self.darknet_width, self.darknet_height),
+                                    interpolation=cv2.INTER_LINEAR)
+        
+        img_for_detect = make_image(
+            self.darknet_width, self.darknet_height, 3)
+        copy_image_from_bytes(
+            img_for_detect, frame_resized.tobytes())
+        return img_for_detect
+
+                
     def inference(self):
+        """Does one inference loop 
 
-        ret, frame = self.cap.read()
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
-        detections, width_ratio, height_ratio = self.darknet_helper(frame, self.width, self.height)
-        return detections
-
-
-
-	
+        Returns
+        -------
+        list
+            a nested list: [[class,cropped_img,x_center],...]
         """
+
+        # Make sure to get a frame
+        while True:
+            ret, frame = self.cap.read()
+            if frame is not None:
+                break
+            if frame is None:
+                print("Didnt catch a frame")
+
+        darknet_image = self.video_capture(frame)
+        prev_time = time.time()
+        detections = detect_image(
+        self.network, self.class_names, darknet_image, thresh=self.thresh)
+        fps = int(1/(time.time() - prev_time))
+        print("FPS: {}".format(fps))
+        print_detections(detections, "store_true")
+        free_image(darknet_image)
+        
+
+        detect_list = []
         for label, confidence, bbox in detections:
-        left, top, right, bottom = bbox2points(bbox)
-        left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
-        cv2.rectangle(image, (left, top), (right, bottom), class_colors[label], 2)
-        cv2.putText(image, "{} [{:.2f}]".format(label, float(confidence)),
-                        (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        class_colors[label], 2)
-        """
+            print(bbox)
+            bbox_adjusted = self.convert2original(frame, bbox)
+            detect_list.append([str(label),self.conv_crop(frame, bbox_adjusted),bbox[0]])
 
-	
+        return detect_list
 	
